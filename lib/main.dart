21736793327409c052d132cf70f986d1feb4f184
anujsingh <anujsingh1317@gmail.com>
2021-12-16 18:14:13 +0530

    Added xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() {
-  runApp(const MaterialApp(home: myapp()));
-}
+void main() => runApp(XylophoneApp());
 
-class myapp extends StatelessWidget {
-  const myapp({Key? key}) : super(key: key);
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color = Colors.red, int soundNumber = 1}) {
+    return Expanded(
+      child: MaterialButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: const Text("Anuj"),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(
-                  "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg"),
-            ),
-            const Text(
-              "Anuj Singh",
-              style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "FLUTTER DEVELOPER",
-              style: TextStyle(
-                  fontSize: 20,
-                  letterSpacing: 2.5,
-                  color: Colors.teal.shade100,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20,
-              width: 200,
-              child: Divider(
-                color: Colors.teal.shade100,
-              ),
-            ),
-            Card(
-              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              color: Colors.white,
-              child: ListTile(
-                leading: const Icon(
-                  Icons.icecream_outlined,
-                  size: 30,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  "1234567891",
-                  style: TextStyle(
-                      fontSize: 18,
-                      letterSpacing: 2.5,
-                      color: Colors.teal.shade100,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            )
-          ],
-        ));
+      ),
+    );
   }
 }
