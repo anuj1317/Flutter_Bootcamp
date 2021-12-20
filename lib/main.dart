cada606f86e0ae4bf430949d299a7c1d4035de7c
anujsingh <anujsingh1317@gmail.com>
2021-12-20 20:32:38 +0530

    Added Weather API with the required functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bootcamp/screens/loading_screen.dart';
 
-void main() {
-  runApp(const MaterialApp(home: myapp()));
-}
+void main() => runApp(MyApp());
 
-class myapp extends StatelessWidget {
-  const myapp({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: const Text("Anuj"),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(
-                  "https://www.cleverfiles.com/howto/wp-content/uploads/2018/03/minion.jpg"),
-            ),
-            const Text(
-              "Anuj Singh",
-              style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "FLUTTER DEVELOPER",
-              style: TextStyle(
-                  fontSize: 20,
-                  letterSpacing: 2.5,
-                  color: Colors.teal.shade100,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20,
-              width: 200,
-              child: Divider(
-                color: Colors.teal.shade100,
-              ),
-            ),
-            Card(
-              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              color: Colors.white,
-              child: ListTile(
-                leading: const Icon(
-                  Icons.icecream_outlined,
-                  size: 30,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  "1234567891",
-                  style: TextStyle(
-                      fontSize: 18,
-                      letterSpacing: 2.5,
-                      color: Colors.teal.shade100,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            )
-          ],
-        ));
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
   }
 }
